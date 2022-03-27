9cb100ebed72c2a5d521b247b1fef2e456fe649d
yihu55 <veronicayi@msn.com>
2022-03-27 23:00:03 +0200

    modified om mig text

--- js/app.js
+++ js/app.js
@@ -1,28 +1,27 @@
 const navSlide = () => {
-    const burger = document.querySelector(".burger")
-    const nav = document.querySelector(".nav-links")
-    const navLinks = document.querySelectorAll(".nav-links li")
-    console.log(navLinks)
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  console.log(navLinks);
 
-    burger.addEventListener("click", () => {
-        //toggle nav
-        nav.classList.toggle("nav-active")
+  burger.addEventListener('click', () => {
+    //toggle nav
+    nav.classList.toggle('nav-active');
 
-        //animation links
-        navLinks.forEach((link, index) => {
-            //link.style.animation = `navLinkFade 0.5s ease forwards ${index/7}s`
-            if (link.style.animation) {
+    //animation links
+    navLinks.forEach((link, index) => {
+      //link.style.animation = `navLinkFade 0.5s ease forwards ${index/7}s`
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+    });
 
-                link.style.animation = ""
-            } else
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+1}s`
-        });
+    //burger animation
+    burger.classList.toggle('toggle');
+  });
+};
 
-        //burger animation
-        burger.classList.toggle("toggle")
-
-
-    })
-}
-
-navSlide()
+navSlide();
